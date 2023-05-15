7d5e91a15b8d07c56aa527ec3d5c04a33ba8b19a
Sugeyth Rodriguez <d.sugeyth02@gmail.com>
2023-05-15 01:44:20 -0600

    :recycle: refactor code

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,3 @@
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 export const Footer = () => {
